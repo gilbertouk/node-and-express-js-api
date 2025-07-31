c31469481d0120928d943fc1e74723119b5d14b6
Gilberto Silva <gilbertoantoniouk@gmail.com>
2025-07-31 11:54:54 +0100

    feat: add markTaskAsCompleted route to tasks API

--- src/routes/v1/tasks/index.ts
+++ src/routes/v1/tasks/index.ts
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { listTasks, getTask, createTask, updateTask } from "./controller";
+import { listTasks, getTask, createTask, updateTask, markTaskAsCompleted } from "./controller";
 import authenticateUser from "@/middleware/authenticate-user";
 import validateRequest from "@/middleware/validate-request";
 import { createTaskSchema, updateTaskSchema } from "@/data/request-schemas";
@@ -11,5 +11,6 @@ tasks.get("/", listTasks);
 tasks.get("/:id", getTask);
 tasks.post("/", validateRequest(createTaskSchema), createTask);
 tasks.put("/:id", validateRequest(updateTaskSchema), updateTask);
+tasks.patch("/:id/complete", markTaskAsCompleted);
 
 export default tasks;
